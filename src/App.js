1df671ba89fd071dec1154b98ed030878df2d260
matuzalema <matuzalema@gmail.com>
2019-05-27 11:11:13 +0200

    Add AddPlayer.js and AddPlayer.test.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlayersList from './components/PlayerList/PlayerList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
  constructor() {
    super();
 
    this.state = {
-     players: [
-       {
-         name: 'Kunegunda',
-         score: 5,
-       },
-       {
-         name: 'Antoś',
-         score: 0,
-       }
-     ]
+     players: []
    }
  }
 
 onScoreUpdate = (playerIndex, scoreChange) => {
+	this.setState({
+		players: this.state.players.map((player, index) => {
+			if (index === playerIndex) {
+				return { ...player, score: player.score + scoreChange };
+			}
+			return player;
+		})
+	})
+}
+
+onPlayerAdd = (playerName) => {
+  const newPlayer = {
+    name: playerName,
+    score: 0,
+  }
   this.setState({
-    players: this.state.players.map((player, index) => {
-      if (index === playerIndex) {
-        return { ...player, score: player.score + scoreChange };
-      }
-      return player;
-    })
+    players: [...this.state.players, newPlayer]
   })
 }
 
- render() {
-   return (
-     <div className="App">
-       <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
-     </div>
-   );
- }
+render(){
+	return (
+		<div className="App">
+			<AddPlayer onPlayerAdd={this.onPlayerAdd} />
+			<PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+		</div>
+		);
+	}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayerList/PlayerList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
+// import Player from './components/Player/Player';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+	shallow(<App />);
+});
+
+it('should update player score', () => {
+	const appComponent = shallow(<App />);
+
+	let players = [
+		{
+			name: 'Kunegunda',
+			score: 5,
+		}
+	];
+
+	appComponent.setState({ players });
+
+	const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+	onScoreUpdate(0, 5);
+	const playersAfterUpdate = appComponent.state('players');
+	expect(playersAfterUpdate[0].score).toEqual(10);
+
+	const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+	onPlayerAdd('Ania');
+	players = appComponent.state('players');
+
+	expect(players.length).toEqual(2);
+	expect(players[0].name).toEqual('Kunegunda');
+	expect(players[0].score).toEqual(5);
 });
