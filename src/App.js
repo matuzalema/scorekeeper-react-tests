a9089f9e582cb607f4840b295b6c2ec0f482c309
matuzalema <matuzalema@gmail.com>
2019-05-27 13:20:22 +0200

    Add the possibility of removing a single player from the list

--- src/App.js
+++ src/App.js
@@ -24,20 +24,25 @@ onScoreUpdate = (playerIndex, scoreChange) => {
 }
 
 onPlayerAdd = (playerName) => {
-  const newPlayer = {
-    name: playerName,
-    score: 0,
-  }
-  this.setState({
-    players: [...this.state.players, newPlayer]
-  })
+	const newPlayer = {
+		name: playerName,
+		score: 0,
+	}
+	this.setState({
+		players: [...this.state.players, newPlayer]
+	})
+}
+
+onPlayerRemove = (playerIndex) => {
+	const newPlayersList = this.state.players.filter((removePlayer, index) => index !==playerIndex);
+	this.setState({players: newPlayersList});
 }
 
 render(){
 	return (
 		<div className="App">
 			<AddPlayer onPlayerAdd={this.onPlayerAdd} />
-			<PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+			<PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} onPlayerRemove={this.onPlayerRemove}/>
 		</div>
 		);
 	}
